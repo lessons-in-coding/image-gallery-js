afbf2a4c531d8cf13d4fc1a2a9c6904cef4eb651
PannaCollege <chancellor@pannacollege.org>
2024-04-29 10:18:51 +0630

    set up images and added to js and initial js setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,112 @@
-window.alert("Hello!");
+// Array of image sources
+var imageSources = [
+  {
+    src: "img1.png",
+    title: "Image 1",
+    description: "Description for Image 1",
+  },
+  {
+    src: "img2.png",
+    title: "Image 2",
+    description: "Description for Image 2",
+  },
+  {
+    src: "img3.png",
+    title: "Image 3",
+    description: "Description for Image 3",
+  },
+  {
+    src: "img4.png",
+    title: "Image 4",
+    description: "Description for Image 4",
+  },
+  {
+    src: "img5.png",
+    title: "Image 5",
+    description: "Description for Image 5",
+  },
+  {
+    src: "img6.png",
+    title: "Image 6",
+    description: "Description for Image 6",
+  },
+  {
+    src: "plant.png",
+    title: "Image 7",
+    description: "Description for Image 7",
+  },
+  {
+    src: "cat.png",
+    title: "Image 8",
+    description: "Description for Image 8",
+  },
+  {
+    src: "dog.png",
+    title: "Image 9",
+    description: "Description for Image 9",
+  },
+  {
+    src: "fish.png",
+    title: "Image 10",
+    description: "Description for Image 10",
+  },
+  {
+    src: "football.png",
+    title: "Image 11",
+    description: "Description for Image 11",
+  },
+  {
+    src: "nerdNote.png",
+    title: "Image 12",
+    description: "Description for Image 12",
+  },
+  {
+    src: "mac.png",
+    title: "Image 13",
+    description: "Description for Image 13",
+  },
+  {
+    src: "linux.png",
+    title: "Image 14",
+    description: "Description for Image 14",
+  },
+  {
+    src: "window.png",
+    title: "Image 15",
+    description: "Description for Image 15",
+  },
+  {
+    src: "git.png",
+    title: "Image 16",
+    description: "Description for Image 16",
+  },
+  {
+    src: "gitBash.png",
+    title: "Image 17",
+    description: "Description for Image 17",
+  },
+  {
+    src: "github.png",
+    title: "Image 18",
+    description: "Description for Image 18",
+  },
+  {
+    src: "gitlab.png",
+    title: "Image 19",
+    description: "Description for Image 19",
+  },
+  {
+    src: "candle1.png",
+    title: "Image 20",
+    description: "Description for Image 20",
+  },
+  {
+    src: "candle2.png",
+    title: "Image 21",
+    description: "Description for Image 21",
+  },
+  // Add more image objects as needed
+];
+
+var currentIndex = 0;
+var totalImages = imageSources.length;
